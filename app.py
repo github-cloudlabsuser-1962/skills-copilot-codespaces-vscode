e384dee296872988c54a5ca077be87189de94ac2
github-cloudlabsuser-1962 <github_cloudlabsuser_1962@cloudlabsaiuser.com>
2024-12-02 21:17:26 +0000

    Copilot second commit

---
+++ app.py
@@ -0,0 +1,10 @@
+def hello():
+    return 'Hello, World'
+
+def prime(n):  # Function to check if a number is prime or not
+    if n <= 1:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
